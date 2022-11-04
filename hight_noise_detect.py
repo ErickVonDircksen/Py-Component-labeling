695044cd6e3c1631220627c6a8f648b2cdb7cfb4
ErickVonDircksen <83781289+ErickVonDircksen@users.noreply.github.com>
2022-11-03 21:52:36 -0300

    Update hight_noise_detect.py

--- hight_noise_detect.py
+++ hight_noise_detect.py
@@ -1,60 +1,80 @@
-
+from math import floor
 import cv2
 import sys
-import matplotlib.pyplot as plt
 import numpy as np
 
-path = '150.bmp'
+
+# Universidade Tecnológica Federal do Paraná
+#===============================================================================
+# Alunos Erick H. Dircksen e André L. Gomes
+
+#entender o conceito,   Check
+#conseguir implementar, not Check
+
+path = '205.bmp'
 
 def noNoise2(img):
  img_out = img.copy()
  kernel = np.ones((3,3),np.uint8)
-
- kernel1 = np.array([[0, 0, 0],
-                     [0, 0, 0],
-                     [0, 0, 0]], np.uint8)
-
+ ddepth = cv2.CV_16S
+ scale = 1
+ delta = 0
  mediumPixel = np.average(img)
- print(mediumPixel)
 
- img_out = cv2.bilateralFilter(img_out, 31,15, 10)
+
+ img_out = cv2.bilateralFilter(img_out, 37,15, 5)
+
+ grad_x = cv2.Sobel(img_out, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+ grad_y = cv2.Sobel(img_out, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+ abs_grad_x = cv2.convertScaleAbs(grad_x)
+ abs_grad_y = cv2.convertScaleAbs(grad_y)
+ img_out = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+ #cv2.imwrite ('01 - pós SOBEL.png',img_out)
+
+ (a,img_out) = cv2.threshold(img,mediumPixel/2,mediumPixel*2,cv2.THRESH_OTSU) 
+
+ #img_out = cv2.adaptiveThreshold(img_out,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,9,14)  
  
- 
- 
- (a,img_out) = cv2.threshold(img,mediumPixel/2,mediumPixel*2,cv2.THRESH_OTSU) # comentar essa linha faz "funcinar nas imagens diferentes da 150.bmp"
-
- cv2.imwrite ('02 - Blur+otsu.png', img_out)
-
- img_out = cv2.adaptiveThreshold(img_out,255,cv2.ADAPTIVE_THRESH_MEAN_C,
-            cv2.THRESH_BINARY,7,14)  
-
- cv2.imwrite ('03 - Blur+otsu+AdapTative.png', img_out) 
+ (a,img_out) = cv2.threshold(img_out,0,255,cv2.THRESH_BINARY)      
 
  img_out  = cv2.morphologyEx(img_out,cv2.MORPH_OPEN,kernel,iterations=1) 
- cv2.imwrite ('04 -Blur+otsu+AdapT+Dilated .png', img_out)
+ #cv2.imwrite ('02 -processedImage.png', img_out)
  return img_out
 
-
+def contaContorno(img,cont):
+ height = img.shape[1]
+ width = img.shape[0]
+ contours = 0
+ 
+ for y in range(height):
+  for x in range(width):
+   b,g,r = (img[x,y])
+   if g > 254:
+        contours += 1
+ 
+ return floor(contours/cont) 
+ 
 
 image = cv2.imread(path, 0)
 if image is None:
         print ('Erro abrindo a imagem.\n')
         sys.exit ()
 
-th3 = noNoise2(image)
+deNoised = noNoise2(image)
 
-canny = cv2.Canny(th3,20, 180, 1)
+canny = cv2.Canny(deNoised,30, 160,1,L2gradient=True,apertureSize=7)
 
-dilated = cv2.dilate(canny, (2,2), iterations = 1)
+dilated = cv2.dilate(canny, (2,2), iterations = 2)
 
-
-
-(cnt, heirarchy) = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+(cnt, hierarchy) = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
 cv2.drawContours(rgb, cnt, -1, (0,255,0), 1)
 
-cv2.imwrite ('01 - edgesDetectec.png', rgb)
+rices = contaContorno(rgb,len(cnt)) 
 
-print('Rice in the image: ', len(cnt))
-#cv2.show()
+print("Maybe there is",rices,"rices in",len(cnt),"blobs")
 
+cv2.imwrite ('02 - edgesDetectec.png', rgb)
+
+print()
